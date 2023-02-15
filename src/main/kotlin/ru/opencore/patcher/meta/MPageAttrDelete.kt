79a82b8315c74be6cb819f65be1596f2fb9e7b64
Ivan Artemov <artemov_i@tii.ru>
2023-02-15 18:51:27 +0300

    Fixed

--- src/main/kotlin/ru/opencore/patcher/meta/MPageAttrDelete.kt
+++ src/main/kotlin/ru/opencore/patcher/meta/MPageAttrDelete.kt
@@ -3,7 +3,7 @@ package ru.opencore.patcher.meta
 import org.jdbi.v3.core.mapper.reflect.ColumnName
 import java.sql.Timestamp
 
-var formatMPageAttrDeleteSqlPostgres = "delete s_mt.t_page_attr " +
+var formatMPageAttrDeleteSqlPostgres = "delete from s_mt.t_page_attr " +
 		"where ck_page = %s;"
 
 data class MPageAttrDelete(@ColumnName("ck_page") val ckPage: String): Meta {
