dfbe9449ccdc2bb4b248fc91667636cf83b6f548
Ivan Artemov <artemov_i@tii.ru>
2025-03-04 23:44:28 +0300

    Fixed

--- src/main/kotlin/ru/opencore/patcher/meta/MQuery.kt
+++ src/main/kotlin/ru/opencore/patcher/meta/MQuery.kt
@@ -3,7 +3,7 @@ package ru.opencore.patcher.meta
 import org.jdbi.v3.core.mapper.reflect.ColumnName
 import java.sql.Timestamp
 
-var formatQuerySqlPostgres = "INSERT INTO s_mt.t_query (ck_id, ck_provider, ck_user, ct_change, cr_type, cr_access, cn_action, cr_cache, cv_cache_key_param, cc_query)%n" +
+var formatQuerySqlPostgres = "INSERT INTO s_mt.t_query (ck_id, ck_provider, ck_user, ct_change, cr_type, cr_access, cn_action, cv_description, cr_cache, cv_cache_key_param, cc_query)%n" +
 		" VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%n %s%n)%n " +
 		"on conflict (ck_id) do update set cc_query = excluded.cc_query, ck_provider = excluded.ck_provider, ck_user = excluded.ck_user, ct_change = excluded.ct_change, cr_type = excluded.cr_type, cr_access = excluded.cr_access, cn_action = excluded.cn_action, cv_description = excluded.cv_description, cr_cache = excluded.cr_cache, cv_cache_key_param = excluded.cv_cache_key_param;"
 
